132a9ee3090ef245d5b98688dd217d61488d821a
sunnixx <sumair.hamza@gmail.com>
2019-02-08 11:54:04 +0500

    lecture 11

---
+++ Lecture 11 - 7 Feb 2019/main.js
@@ -0,0 +1,57 @@
+// Callback functions
+
+// function foo(cb) {
+//     cb() //Invoking a callback
+// }
+
+// foo(function() {
+//     //Anonymous Functions
+//     console.log("Callback from Foo")
+// });
+
+// function foo(num,cb) {
+//     //Random number generator 
+//     let randomNumber = Math.floor(Math.random() * num);
+
+//     cb(randomNumber)
+// }
+
+// foo(40,function(randomGeneratedNumber) {
+//     console.log(randomGeneratedNumber)
+//     console.log(Math.sin(randomGeneratedNumber))
+// })
+
+//Client - Server 
+//Server -> value retrieve (Username & Password -> True => Token)
+//Client -> Through which you can view protected pages
+
+// document.getElementById('btn').addEventListener('click',function() {
+//     alert('Hello World')
+// })
+
+//Asynchronous and Synchronous 
+// console.log('1')
+// console.log('2')
+// console.log('3')
+
+// setTimeout(function(){
+//     console.log('4')
+//     console.log('5')
+// },2000)
+
+// console.log('6')
+// console.log('7')
+
+// setInterval(function() {
+//     console.log('Interval')
+// },2000)
+
+// console.log('1')
+// console.log('2')
+
+// setTimeout(function() {
+//     prompt("Enter anything")
+// })
+
+// console.log('3')
+// console.log('4')
